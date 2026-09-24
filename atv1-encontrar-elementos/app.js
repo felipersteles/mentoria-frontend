c681450771b27e659d830f84e1fa97b6e4dbedff
wesley <weley.academico@proton.me>
2026-09-24 11:45:01 -0300

    atv1: encontrar elementos

--- atv1-encontrar-elementos/app.js
+++ atv1-encontrar-elementos/app.js
@@ -30,6 +30,6 @@ const avisoSelecao = document.querySelector("#aviso-selecao");
 // Se algum seletor falhar, mostre uma mensagem em "avisoSelecao"
 // (remova o atributo "hidden" e use textContent) explicando qual seletor
 // não encontrou nada. Se estiver tudo certo, "avisoSelecao" deve
-// continuar escondido (hidden).
+// continuar escondido (hidden). d
 
 console.log({ titulo, linksDoMenu, secaoServicos, botaoEnviar });
