e63ea421014815f0ff4977e6a591bf50036e4504
wesley <weley.academico@proton.me>
2026-09-24 11:39:37 -0300

    Projeto Final

--- projeto-final-login/js/main.js
+++ projeto-final-login/js/main.js
@@ -40,14 +40,29 @@ form.addEventListener("submit", (evento) => {
   // dois estiver vazio, chame mostrarErro(campo, elementoErro, "mensagem")
   // para o campo certo (campoEmail/erroEmail ou campoSenha/erroSenha) e
   // use "return" para interromper a função.
+  if (email === null || email === "") {
+    let elementoEmail = document.getElementById("erro-email");
+    mostrarErro(campoEmail, elementoEmail, "E-Mail não pode estar vazio.");
+  }
+  if (senha === null || senha === "") {
+    let elementoSenha = document.getElementById("erro-senha");
+    mostrarErro(campoSenha, elementoSenha, "Senha não pode estar vazia.");
+  }
 
   // TODO 2: compare "email" e "senha" com USUARIO_VALIDO.email e
   // USUARIO_VALIDO.senha. Se não baterem, chame
   // mostrarErro(campoSenha, erroSenha, "E-mail ou senha inválidos")
   // e "return".
+  if (email !== USUARIO_VALIDO.email && senha !== USUARIO_VALIDO.senha) {
+    mostrarErro(campoSenha, erroSenha, "E-mail ou senha inválidos")
+  } else {
+    sessionStorage.setItem(CHAVE_SESSAO, email)
+    window.location.href = "dash.html"
+  }
 
   // TODO 3: se as credenciais estiverem certas, salve o e-mail em
   // sessionStorage (sessionStorage.setItem(CHAVE_SESSAO, email)) e
   // redirecione o navegador para "dash.html" usando
   // window.location.href = "dash.html".
+
 });
